f29b93fe5c888bf282765a345ca7abfbbc8cda2b
eltiness <147994741+eltiness@users.noreply.github.com>
2023-10-15 18:20:26 +0300

    Add files via upload

---
+++ Лаб1/Lab1_task3.py
@@ -0,0 +1,8 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+
+# TODO Разделите участников на две команды
+half=len(list_players)//2
+team_1=list_players[:half]
+team_2=list_players[half:]
+print(team_1)
+print (team_2)
